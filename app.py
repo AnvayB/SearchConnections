132b36cc1da8f02be8f8232149d01ed3cde2d906
Promtp One User <user@promptone.at>
2025-07-24 18:01:18 -0700

    removed email addresses

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ app = Flask(__name__)
 # Load the data
 def load_data():
     anvay = pd.read_csv('./files/anvay-connections.csv')
-    anil_connections = pd.read_csv('./files/anil-connections.csv')
-    bilwa_connections = pd.read_csv('./files/bilwa-connections.csv')
+    anil = pd.read_csv('./files/anil-connections.csv')
+    bilwa = pd.read_csv('./files/bilwa-connections.csv')
     
-    # Drop email columns from all dataframes
-    anvay = anvay.drop('Email Address', axis=1)
-    anil_connections = anil_connections.drop('Email Address', axis=1)
-    bilwa_connections = bilwa_connections.drop('Email Address', axis=1)
-    
-    return anvay, anil_connections, bilwa_connections
+    return anvay, anil, bilwa
 
 @app.route('/')
 def index():
@@ -28,12 +23,12 @@ def search():
     if not company_name:
         return jsonify({'error': 'Please provide a company name'})
     
-    anvay, anil_connections, bilwa_connections = load_data()
+    anvay, anil, bilwa = load_data()
     
     # Search in all datasets
     anvay_matches = anvay[anvay['Company'].str.lower() == company_name.lower()]
-    anil_matches = anil_connections[anil_connections['Company'].str.lower() == company_name.lower()]
-    bilwa_matches = bilwa_connections[bilwa_connections['Company'].str.lower() == company_name.lower()]
+    anil_matches = anil[anil['Company'].str.lower() == company_name.lower()]
+    bilwa_matches = bilwa[bilwa['Company'].str.lower() == company_name.lower()]
     
     # Filter anil matches for Closeness 1.0 or 2.0
     filtered_anil_matches = anil_matches[anil_matches['Closeness'].isin([1.0, 2.0])]
@@ -55,12 +50,12 @@ def search():
 
 @app.route('/companies')
 def get_companies():
-    anvay, anil_connections, bilwa_connections = load_data()
+    anvay, anil, bilwa = load_data()
     
     # Get unique companies from all datasets
     anvay_companies = set(anvay['Company'].dropna().unique())
-    anil_companies = set(anil_connections['Company'].dropna().unique())
-    bilwa_companies = set(bilwa_connections['Company'].dropna().unique())
+    anil_companies = set(anil['Company'].dropna().unique())
+    bilwa_companies = set(bilwa['Company'].dropna().unique())
     
     # Combine and sort
     all_companies = sorted(list(anvay_companies.union(anil_companies).union(bilwa_companies)))
